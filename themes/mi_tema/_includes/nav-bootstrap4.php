ae24958b50dd7325d4401d91e189f154f7f7d5d0
Bernardita <bernardita.fono@gmail.com>
2019-10-16 14:22:56 -0300

    menu autoadministrable

--- themes/mi_tema/_includes/nav-bootstrap4.php
+++ themes/mi_tema/_includes/nav-bootstrap4.php
@@ -1,18 +1,18 @@
-<nav class="navbar navbar-expand-md navbar-light bg-light" role="navigation">
+<nav class="navbar navbar-expand-lg fixed-top" role="navigation">
 	<div class="container">
 		<!-- Brand and toggle get grouped for better mobile display -->
-		<a class="navbar-brand" href="<?php bloginfo('url') ?>">Navbar</a>
-		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1" aria-expanded="false" aria-label="Toggle navigation">
-			<span class="navbar-toggler-icon"></span>
+		<a class="navbar__logo" href="<?php bloginfo('url') ?>"><img class="navbar__img" src="<?php echo get_theme_file_uri(); ?>/assets/images/logo.png" alt="logo"></a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+			<i class="fas fa-bars"></i>
 		</button>
 
-		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+		<div class="collapse navbar-collapse" id="navbarNav">
 		<?php if ( has_nav_menu( 'header-menu' ) ) { ?>
 			<?php wp_nav_menu( array(
 				'theme_location'	=> 'header-menu',
 				'depth'				=> 2,
-				'container'			=> false,
-				'menu_class'		=> 'navbar-nav mr-auto',
+				'container'			=> true,
+				'menu_class'		=> 'navbar-nav ml-auto',
 				'fallback_cb'		=> 'WP_Bootstrap_Navwalker::fallback',
 				'walker'			=> new WP_Bootstrap_Navwalker(),
 			) ); ?>
--- themes/mi_tema/header.php
+++ themes/mi_tema/header.php
@@ -9,39 +9,5 @@
 </head>
 <body>
 
-	<nav class="navbar navbar-expand-lg fixed-top">
-				<div class="container">
-					<a class="navbar__logo" href="index.html"> <img class="navbar__img" src="<?php echo get_theme_file_uri(); ?>/assets/images/logo.png" alt="logo"> </a>
 
-					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-						<i class="fas fa-bars"></i>
-					</button>
-
-					<div class="collapse navbar-collapse" id="navbarNav">
-						<ul class="navbar-nav ml-auto">
-							<li class="nav-item">
-								<a class="nav-link" href="inicio">Inicio</a>
-							</li>
-							<li class="nav-item">
-								<a class="nav-link" href="rutas">Rutas</a>
-							</li>
-							<li class="nav-item">
-								<a class="nav-link" href="galeria">Galería</a>
-							</li>
-							<li class="nav-item">
-								<a class="nav-link" href="contacto">Contacto</a>
-							</li>
-						</ul>
-					</div>
-				</div>
-	</nav>
-
-	<?php if ( has_nav_menu( 'header-menu' ) ) { ?>
-		<?php wp_nav_menu( array(
-			'theme_location' => 'header-menu',
-			'container_class' => 'header-menu'
-		) ); ?>
-
-
-
-	<?php } ?>
+	<?php get_template_part('_includes/nav', 'bootstrap4') ?>
