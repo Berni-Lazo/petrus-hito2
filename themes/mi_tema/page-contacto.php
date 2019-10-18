89492009ff868768747a30a0f755bad7bfac239d
Bernardita <bernardita.fono@gmail.com>
2019-10-18 00:38:57 -0300

    main pages styles

--- themes/mi_tema/page-contacto.php
+++ themes/mi_tema/page-contacto.php
@@ -1,26 +1,27 @@
 <?php get_header() ?>
-<h2 class="contact__title">Contacto</h2>
-	<p class="contact__description">Para reservas o mayor información contáctanos en:</p>
+<main class="contact">
+	<h2 class="contact__title">Contacto</h2>
+		<p class="contact__description">Para reservas o mayor información contáctanos en:</p>
 
-	<div class="contact__box container">
-		<div class="contact__icons">
-			<div class="">
-				<i class="contact__icon fab fa-whatsapp"></i> <p class="contact__info"> +56 9 9316921</p>
+		<div class="contact__box container">
+			<div class="contact__icons">
+				<div class="">
+					<i class="contact__icon fab fa-whatsapp"></i> <p class="contact__info"> +56 9 9316921</p>
+				</div>
+				<div class="">
+					<i class="contact__icon_2 fab fa-facebook"></i> <p class="contact__info"> PetrusBirdwatching</p>
+				</div>
+				<div class="">
+					<i class="contact__icon_3 fab fa-instagram"></i> <p class="contact__info"> Petrus_Birdwatching_Patagonia</p>
+				</div>
+				<div class="">
+					<i class="contact__icon_4 far fa-envelope"></i> <p class="contact__info"> petrusbirdwatching@gmail.com</p>
+				</div>
 			</div>
-			<div class="">
-				<i class="contact__icon_2 fab fa-facebook"></i> <p class="contact__info"> PetrusBirdwatching</p>
-			</div>
-			<div class="">
-				<i class="contact__icon_3 fab fa-instagram"></i> <p class="contact__info"> Petrus_Birdwatching_Patagonia</p>
-			</div>
-			<div class="">
-				<i class="contact__icon_4 far fa-envelope"></i> <p class="contact__info"> petrusbirdwatching@gmail.com</p>
+			<div class="contact__form">
+				<?php the_post() ?>
+				<?php the_content(); ?>
 			</div>
 		</div>
-		<div class="contact__form">
-			<?php the_post() ?>
-			<?php the_content(); ?>
-		</div>
-	</div>
-
+</main>
 <?php get_footer() ?>
--- themes/mi_tema/page-galeria.php
+++ themes/mi_tema/page-galeria.php
@@ -1,6 +1,9 @@
 <?php get_header() ?>
+<main class="gallery">
   <h2 class="gallery__title">Galería</h2>
   <?php the_post() ?>
 
   <?php if( function_exists('photo_gallery') ) { photo_gallery(7); } ?>
+</main>
+
 <?php get_footer() ?>
