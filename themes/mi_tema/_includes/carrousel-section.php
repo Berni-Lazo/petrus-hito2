325789e093e69ec0dd441f1fcd802068595bd979
Bernardita <bernardita.fono@gmail.com>
2019-10-18 21:42:12 -0300

    template-part

---
+++ themes/mi_tema/_includes/carrousel-section.php
@@ -0,0 +1,52 @@
+<section class="bd-example">
+        <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+          <ol class="carousel-indicators">
+            <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+            <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+            <li data-target="#carouselExampleCaptions" data-slide-to="3"></li>
+          </ol>
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img src="<?php echo get_theme_file_uri(); ?>/assets/images/slide1.jpg" class="img-carousel d-block w-100" alt="foto1">
+              <div class="carousel-caption">
+                <h5 class="carousel-caption__title">Una experiencia única</h5>
+                <p class="carousel-caption__description d-none d-lg-block">Recorre la Carretera Austral en manos de personal entrenado para atenderte y ayudarte en lo necesario para que tu experiencia sea inolvidable</p>
+                <a class="carousel-caption__button" href="rutas">Conoce nuestras rutas</a>
+              </div>
+            </div>
+            <div class="carousel-item">
+              <img src="<?php echo get_theme_file_uri(); ?>/assets/images/slide2.jpg" class="img-carousel d-block w-100" alt="foto2">
+              <div class="carousel-caption">
+                <h5 class="carousel-caption__title">Turismo con acceso para todos</h5>
+                <p class="carousel-caption__description d-none d-lg-block">La flora, la fauna y los paisajes únicos de la Patagonia Chilena están al alcance de todos.</p>
+                <a class="carousel-caption__button" href="contacto">Reserva ahora</a>
+              </div>
+            </div>
+            <div class="carousel-item">
+              <img src="<?php echo get_theme_file_uri(); ?>/assets/images/slide3.jpg" class="img-carousel d-block w-100" alt="foto3">
+              <div class="carousel-caption">
+                <h5 class="carousel-caption__title">Fotografía especializada</h5>
+                <p class="carousel-caption__description d-none d-lg-block">Descubre y registra la riqueza única que existe en la gran variedad de aves que habitan la Región de Aysén</p>
+                <a class="carousel-caption__button" href="galeria">Mira nuestras fotos</a>
+              </div>
+            </div>
+            <div class="carousel-item">
+              <img src="<?php echo get_theme_file_uri(); ?>/assets/images/slide4.jpg" class="img-carousel d-block w-100" alt="foto4">
+              <div class="carousel-caption">
+                <h5 class="carousel-caption__title">La Patagonia te está esperando</h5>
+                <p class="carousel-caption__description d-none d-lg-block">No lo pienses más y únete a la aventura</p>
+                <a class="carousel-caption__button" href="contacto">Contáctanos</a>
+              </div>
+            </div>
+          </div>
+          <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Anterior</span>
+          </a>
+          <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Siguiente</span>
+          </a>
+        </div>
+      </section>
